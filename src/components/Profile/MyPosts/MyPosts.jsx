52b1b8de2de14b8fef23b39fd8e91f762a2e8ab2
Fogel Egor <fogel_92@bk.ru>
2022-07-26 14:51:48 +0700

    feat: add function onClick

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,26 @@
+import React from 'react';
 import styles from './MyPosts.module.css';
 import Post from './Post/Post';
 
 function MyPosts(props) {
-const postElements = props.postsData.map(post => <Post message={post.message} likesCount={post.likesCount} />)
+  const postElements = props.postsData.map((post) => (
+    <Post message={post.message} likesCount={post.likesCount} />
+  ));
+
+  const newPostElement = React.createRef();
+
+  const addPost = () => {
+    alert(newPostElement.current.value);
+  };
 
   return (
     <div>
       My posts
       <div>
-        <textarea></textarea>
-        <button>Add Post</button>
+        <textarea ref={newPostElement}></textarea>
+        <button onClick={addPost}>Add Post</button>
       </div>
-      <div className={styles.posts}>
-        {postElements}
-      </div>
+      <div className={styles.posts}>{postElements}</div>
     </div>
   );
 }
