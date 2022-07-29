719ab98ae29358f5dfc2dd73c74a4ae3e16cbb84
Fogel Egor <fogel_92@bk.ru>
2022-07-30 01:43:19 +0700

    feat: added copy of state in profileReducer

--- src/index.js
+++ src/index.js
@@ -7,18 +7,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-export const rerenderEntireTree = () => {
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>,
-  );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(rerenderEntireTree);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,31 +1,41 @@
 const initialState = {
   postsData: [
-    {id: 3, message: 'How are you?', likesCount: 8},
-    {id: 2, message: 'Hello, World', likesCount: 10},
-    {id: 1, message: 'Its my first post!', likesCount: 3},
+    { id: 3, message: 'How are you?', likesCount: 8 },
+    { id: 2, message: 'Hello, World', likesCount: 10 },
+    { id: 1, message: 'Its my first post!', likesCount: 3 },
   ],
   newPostText: '',
 };
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD-POST' :
+    case 'ADD-POST': {
       const newPost = {
-        id: 6, message: state.newPostText, likesCount: 0,
+        id: 6,
+        message: state.newPostText,
+        likesCount: 0,
       };
-      state.postsData.unshift(newPost);
-      state.newPostText = '';
-      return state;
-    case 'UPDATE-POST-TEXT' :
-      state.newPostText = action.newText;
-      return state;
+      const stateCopy = { ...state };
+      stateCopy.postsData = [...state.postsData];
+      stateCopy.postsData.unshift(newPost);
+      stateCopy.newPostText = '';
+      return stateCopy;
+    }
+    case 'UPDATE-POST-TEXT': {
+      const stateCopy = { ...state };
+      stateCopy.newPostText = action.newText;
+      return stateCopy;
+    }
     default:
       return state;
   }
-}
+};
 
 export default profileReducer;
 
-export const addPostActionCreator = () => ({type: 'ADD-POST'});
+export const addPostActionCreator = () => ({ type: 'ADD-POST' });
 
-export const updateNewPostTextActionCreator = (text) => ({type: 'UPDATE-POST-TEXT', newText: text});
+export const updateNewPostTextActionCreator = (text) => ({
+  type: 'UPDATE-POST-TEXT',
+  newText: text,
+});
