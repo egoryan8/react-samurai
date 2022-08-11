de7ef4013033cf471e8da11705cdf8c27cecbb5a
Fogel Egor <fogel_92@bk.ru>
2022-08-11 15:31:12 +0700

    feat: add react-fonal-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
+import styles from './Login.module.css';
+
+function LoginForm() {
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit }) => (
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div>
+            <Field name="login" placeholder={'Login'} component="input" />
+          </div>
+          <div>
+            <Field name="password" type="password" placeholder={'Password'} component="input" />
+          </div>
+          <div className={styles.container}>
+            <Field id="rememberMe" name="rememberMe" type="checkbox" component="input" />
+            <label for="rememberMe">remember me</label>
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      )}
+    />
+  );
+}
 
 function Login() {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <h1>LOGIN</h1>
+      <LoginForm />
     </div>
   );
 }
